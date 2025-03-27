6f59e606f0968d6d68d511b9b0a7a342266605f5
Jihad Chaalan <chaalan2004@gmail.com>
2025-03-27 11:06:20 +0200

    Start working with eloquent model

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,16 +7,17 @@
 <div class="card mt-4">
     <h5 class="card-header">Post Info</h5>
     <div class="card-body">
-        <h5 class="card-title">Title: {{$post['title']}}</h5>
-        <p class="card-text">Description: {{$post['description']}}</p>
+        <h5 class="card-title">Title: {{$post->title}}</h5>
+        <p class="card-text">Description: {{$post->description}}</p>
     </div>
 </div>
 <div class="card mt-4">
+    @dd($post -> user )
     <h5 class="card-header">Post Creator Info</h5>
     <div class="card-body">
-        <h5 class="card-title">Name: {{$post['posted_by']}}</h5>
-        <p class="card-text">Email: hhhh@gmail.com</p>
-        <p class="card-text">Created At: {{$post['created_at']}}</p>
+        <h5 class="card-title">Name: {{$post-> user -> name}}</h5>
+        <p class="card-text">Email: {{$post-> user -> email}}</p>
+        <p class="card-text">Created At: {{$post -> user -> created_at}}</p>
     </div>
 </div>
 
